a3bfa0ba23b09b146859cd5058e5b23824532cfd
kimmonti <kim.montiel@gmail.com>
2019-07-19 18:44:31 -0400

    Carpeta de desarrollo de pagina web

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Ara_project_web.models import Datos
+from .models import Datos
+
+def home(request):
+    return HttpResponse("<h1 Style = 'color': black> Homepage </h1>")
+    #listita = Datos.objects.all() #consultamos la BD y guardamos todos
+    #return render(request, 'x.html', listita) #Los datos se agregan despues del html
 
 
 
