7f3f8a99b1ccd7af2241f09045b80f2de52b0335
kimmonti <kim.montiel@gmail.com>
2019-07-19 18:45:13 -0400

    Carpeta de instalacion

--- Set_up/urls.py
+++ Set_up/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from Ara_project_web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.home),
 ]
