6bec43ae1ef7fed47628ac5dbbf10bd7998dab7f
Marcelo Elizeche <melizeche@gmail.com>
2019-07-19 17:09:46 -0400

    Carpeta de desarrollo de pagina web

---
+++ Ara_project_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class APW_Config(AppConfig): #APW es Ara project web
+    name = 'Ara Project Web'
---
+++ Ara_project_web/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from Ara_project_web.models import Datos
+
+
+
