f377fb69c26a09007465097e5bed5c48f71c859a
Ibrahim Jalife <jalifeabraham9@gmail.com>
2019-07-20 12:56:07 -0400

    Se agregaron las imagenes redimensionadas

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Datos
 
 def home(request):
+    #listita = Datos.objects.all() #consultamos la BD y guardamos todos
     return render(request, 'home.html') #Los datos se agregan despues del html
 
 
