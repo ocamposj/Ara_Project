36dd8af697e8fce3074d4d1ca2acdae58bfe82e4
Ibrahim Jalife <jalifeabraham9@gmail.com>
2019-07-20 14:03:26 -0400

    subida de archivos html

---
+++ Ara_project_web/static/img/resize.py
@@ -0,0 +1,13 @@
+
+import unicodedata 
+
+from PIL import Image
+from resizeimage import resizeimage 
+
+lista = ["education.jpg", "kidswithcomputer.jpg"]
+
+for titulo in lista:
+    with open(titulo, 'r+b') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, [600, 600])
+            cover.save(titulo, image.format)
