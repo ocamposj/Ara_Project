f6f62bd933d00b204f54c7b48babebe8cc618fb8
Alexandra1996-web <Alexandra1996-web>
2019-07-21 17:24:42 -0400

    funciono jeje

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Datos
+#from .models import Datos
+import json
 
 def home(request):
     #listita = Datos.objects.all() #consultamos la BD y guardamos todos
     return render(request, 'home.html') #Los datos se agregan despues del html
 
 def data_from_sensor(request):
-    with open("datos.json", "r") as archivo:
-        resultados = archivo.readlines()
+    with open("/home/pi/Desktop/Ara_Project/Ara_project_web/datos.json", "r") as archivo:
+        datos = json.load(archivo)
+        print (datos) #FUNCIONA :')-
 
-    return render(request, "sensor_data.html", resultados)
+    return render(request, "sensor_data.html", datos)
+
