5bc09d4b9c5b8c0749bea2292afe5e6789eff63a
Jose Ocampos <jose.ocampos.1618@gmail.com>
2019-07-21 17:37:39 -0400

    Se redirigen los datos a home.html

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -5,12 +5,6 @@ import json
 
 def home(request):
     #listita = Datos.objects.all() #consultamos la BD y guardamos todos
-    return render(request, 'home.html') #Los datos se agregan despues del html
-
-def data_from_sensor(request):
     with open("/home/pi/Desktop/Ara_Project/Ara_project_web/datos.json", "r") as archivo:
-        datos = json.load(archivo)
-        print (datos) #FUNCIONA :')-
-
-    return render(request, "sensor_data.html", datos)
-
+        datos = json.load(archivo) #FUNCIONA :')-
+    return render(request, 'home.html', datos) #Los datos se agregan despues del html
