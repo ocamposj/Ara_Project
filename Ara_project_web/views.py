2a540baa8e8dec1cdd9759567cded3ff4bf94012
Bryan Giger <bryan@gigerbyt.es>
2019-07-23 13:03:33 -0400

    create endpoint

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 #from .models import Datos
 import json
 
@@ -8,3 +10,9 @@ def home(request):
     with open("/home/pi/Desktop/Ara_Project/datos.json", "r") as archivo:
         datos = json.load(archivo) #FUNCIONA :')-
     return render(request, 'home.html', datos) #Los datos se agregan despues del html
+@csrf_exempt
+def add_data(request):
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        print(json_data)
+    return HttpResponse(status=200)
