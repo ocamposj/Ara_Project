26196f86eea595f931da3263d42f20d0b9e47c3b
Alex Caceres <alexcaceresfca@gmail.com>
2019-07-23 15:02:48 -0400

    Change source of data to database sqlite

--- Ara_project_web/views.py
+++ Ara_project_web/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-#from .models import Datos
+from .models import Datos
 import json
 import sqlite3
 
 def home(request):
-    #listita = Datos.objects.all() #consultamos la BD y guardamos todos
-    with open("/home/pi/Desktop/Ara_Project/datos.json", "r") as archivo:
-        datos = json.load(archivo) #FUNCIONA :')-
-    return render(request, 'home.html', datos) #Los datos se agregan despues del html
+    info = Datos.objects.last()
+    Tempe = info.temperatura
+    Hume = info.humedad
+    dict = {"Temperatura":Tempe,"Humedad":Hume}
+    print("tempe",Tempe, "hume",Hume) #consultamos la BD y guardamos todos
+    return render(request, 'home.html', dict) #Los datos se agregan despues del html
 
 @csrf_exempt
 def add_data(request):
