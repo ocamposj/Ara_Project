418d0912b08b3d9901b8e03019cc49be49482ec5
Jose Ocampos <jose.ocampos.1618@gmail.com>
2019-07-23 16:19:45 -0400

    Ojala que funcione ahora

--- Set_up/settings.py
+++ Set_up/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4e%&r8p)r30m0-23ek+x$0(+!fiuxx0i+ml0l@^1gv1f(a00ea'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,13 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- datos.py
+++ datos.py
@@ -5,21 +5,40 @@ import datetime
 #import data_sqlite
 import sqlite3
 
+
 try:
 	data_sqlite.crear_database()
 except:
 	pass
 
+
 #conn=data_sqlite.crear_conexion("database1.db")	
+	
+led_r = 23
+
+led_y = 24
+
+led_g = 25
 
 gpio.setmode(gpio.BCM)
+
+gpio.setup(led_r,gpio.OUT)
+
+gpio.setup(led_y,gpio.OUT)
+
+gpio.setup(led_g,gpio.OUT)
+
+
+led_list = [led_r,led_y,led_g]
+
+led_time = 0.05
 
 def DHT11_data():
 
 	humi, temp = dht.read_retry(dht.DHT11, 18)
-	time = datetime.datetime.now()
+	date_time = datetime.datetime.now()
 	
-	return humi, temp, time
+	return humi, temp, 0
 
 while True:
 
@@ -28,14 +47,32 @@ while True:
 		humedad, temperatura, tiempo = DHT11_data()
 		datos = {"Temperatura": temperatura, "Humedad": humedad, "tiempo": tiempo} #le digo que me escriba la temp y humed diccionario
 
+		with open("datos.json", "w") as archivo: # Escribe el archivo de datos
+			archivo.writelines([str(datos).replace("'",'"')])	# Escribe el diccionario 'datos'
+
+		print("Humedad", humedad, "Temperatura", temperatura)
 		sql = "INSERT INTO Ara_project_web_datos (temperatura, humedad) values ({0},{1});".format(temperatura, humedad)
 		print(sql)                       
 		conn=sqlite3.connect("db.sqlite3")
 		with conn:
 			cur = conn.cursor()
 			cur.execute(sql)
+		
+		
+		if humedad > 50.0:
 
-		sleep(15)
+			gpio.output(led_r, True)
+
+			gpio.output(led_g, False)
+
+		else:
+
+			gpio.output(led_r, False)
+
+			gpio.output(led_g, True)
+
+		sleep(10)
+		
 
 	except KeyboardInterrupt:
 
@@ -44,3 +81,4 @@ while True:
 		print("Programa terminado")
 
 		break
+
