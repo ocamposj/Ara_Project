c63e180815cee30566f24f80087e36601a04c9ff
Alexandra1996-web <Alexandra1996-web>
2019-07-22 20:47:03 -0400

    Trasladamos las funciones de db a datos. La db sera por definicion la de django. Necesidad de sql para escribir y testear los comandos.

--- Ara_project_web/models.py
+++ Ara_project_web/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Datos (models.Model):
-    temperatura = models.CharField(max_length = 100)
-    presion_atm = models.CharField(max_length = 100)
-    humedad = models.CharField(max_length = 100)
-    dir_viento = models.CharField(max_length = 100)
-    creado = models.DateTimeField(auto_now_add=True)
+    temperatura = models.CharField(max_length = 100, blank=True, null=True)
+    presion_atm = models.CharField(max_length = 100, blank=True, null=True)
+    humedad = models.CharField(max_length = 100, blank=True, null=True)
+    dir_viento = models.CharField(max_length = 100, blank=True, null=True)
+    creado = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Datos"
 
 
 
